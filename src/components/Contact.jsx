b74307656d5ee0e9242c1899f14d02f7ce67923c
om wankhede <140480438+omwankhede8@users.noreply.github.com>
2024-10-26 15:33:03 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -86,15 +86,7 @@ const Contact = () => {
             </div>
             <div className="flex justify-center pb-6 lg:pl-36 lg:pr-36  ">
               {/* Map */}
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d45456.618606430566!2d78.17776009101199!3d26.213456496260445!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3976c5d1792291fb%3A0xff4fb56d65bc3adf!2sGwalior%2C%20Madhya%20Pradesh!5e0!3m2!1sen!2sin!4v1711173336014!5m2!1sen!2sin"
-                width="100%"
-                height="350"
-                style={{ border: 0 }}
-                allowFullScreen=""
-                loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"
-              ></iframe>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15555.9650040584!2d77.60229029613026!3d12.908283663976707!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae14fc4200bfe1%3A0x987ebf87e35428b5!2sBTM%202nd%20Stage%2C%20BTM%20Layout%2C%20Bengaluru%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1729936891402!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
               {/* Map */}
             </div>
           </section>
